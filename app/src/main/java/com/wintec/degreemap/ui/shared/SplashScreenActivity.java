7b95c6e98a2e3104be43fe99163e3b7e994f0ce3
Ynel Basa <marinelbasa@gmail.com>
2020-11-02 22:41:03 +1300

    Save tutorial status on shared preference

--- app/src/main/java/com/wintec/degreemap/ui/shared/SplashScreenActivity.java
+++ app/src/main/java/com/wintec/degreemap/ui/shared/SplashScreenActivity.java
@@ -3,10 +3,14 @@ package com.wintec.degreemap.ui.shared;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.wintec.degreemap.R;
+
+import static com.wintec.degreemap.ui.shared.TutorialPart2.KEY_TUTORIAL_DONE;
+import static com.wintec.degreemap.ui.shared.TutorialPart2.SHARED_PREFERENCES;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -15,11 +19,14 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        final boolean isTutorialDone = prefs.getBoolean(KEY_TUTORIAL_DONE, false);
+
         // Method to show splash screen for 3 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, TutorialPart1.class);
+                Intent i = new Intent(SplashScreenActivity.this, isTutorialDone ? RoleSelectionActivity.class : TutorialPart1.class);
                 startActivity(i);
                 finish();
             }
