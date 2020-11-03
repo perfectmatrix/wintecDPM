73c04166f118ff04e02026eef18e262b5e117d16
Jingyi Niu <niu.jy1203@gmai.com>
2020-11-03 21:02:41 +1300

    update check box icon

--- app/src/main/java/com/wintec/degreemap/ui/student/student_courses/CourseAdapter.java
+++ app/src/main/java/com/wintec/degreemap/ui/student/student_courses/CourseAdapter.java
@@ -85,7 +85,7 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
         // Show check icon on completed modules
         boolean isModuleCompleted = mCompletedModules.contains(mSelectedCourse.getKey());
         if (isModuleCompleted) {
-            holder.courseNameTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_check_box,0);
+            holder.courseNameTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_check_circle,0);
         }
 
         holder.courseCard.setOnClickListener(this);
