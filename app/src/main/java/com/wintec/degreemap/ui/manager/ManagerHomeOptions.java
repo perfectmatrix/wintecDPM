7350b0a1e5101658d9338af0d0723ef34ae75339
Jingyi Niu <niu.jy1203@gmai.com>
2020-11-04 20:33:42 +1300

    update header layouts

--- app/src/main/java/com/wintec/degreemap/ui/manager/ManagerHomeOptions.java
+++ app/src/main/java/com/wintec/degreemap/ui/manager/ManagerHomeOptions.java
@@ -18,6 +18,6 @@ public class ManagerHomeOptions extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_manager_home_options, container, false);
+        return inflater.inflate(R.layout.fragment_manager_dashboard, container, false);
     }
 }
