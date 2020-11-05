ad5d13f01520ebc90ab211ba17d1375dd90f3c48
Subash Karki <46985121+subashkarki@users.noreply.github.com>
2020-11-05 21:41:09 +1300

    Revert "Work on edit course binding and navigation"
    This reverts commit 34e6948338457e9356cef577d019ed3ded85a9f0.

--- app/src/main/java/com/wintec/degreemap/ui/manager/manage_courses/ManagerCourseDetailsFragment.java
+++ app/src/main/java/com/wintec/degreemap/ui/manager/manage_courses/ManagerCourseDetailsFragment.java
@@ -5,14 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.wintec.degreemap.R;
 import com.wintec.degreemap.data.model.Course;
@@ -20,11 +17,10 @@ import com.wintec.degreemap.viewmodel.CourseViewModel;
 
 import static android.content.Context.MODE_PRIVATE;
 import static com.wintec.degreemap.util.Constants.BUNDLE_COURSE_CODE;
-import static com.wintec.degreemap.util.Constants.BUNDLE_PATHWAY;
 import static com.wintec.degreemap.util.Constants.SHARED_PREFERENCES;
 import static com.wintec.degreemap.util.Helpers.getPathwayLabel;
 
-public class ManagerCourseDetailsFragment extends Fragment implements View.OnClickListener {
+public class ManagerCourseDetailsFragment extends Fragment {
     TextView courseCodeTextView,
             courseLongNameTextView,
             courseLevelTextView,
@@ -33,9 +29,6 @@ public class ManagerCourseDetailsFragment extends Fragment implements View.OnCli
             coRequisiteTextView,
             pathwayTextView,
             courseDescriptionTextView;
-
-    Button btnEdit, btnDelete;
-
     private Course mSelectedCourse;
     SharedPreferences mPrefs;
 
@@ -51,11 +44,6 @@ public class ManagerCourseDetailsFragment extends Fragment implements View.OnCli
         coRequisiteTextView = view.findViewById(R.id.coRequisiteTextView);
         pathwayTextView = view.findViewById(R.id.pathwayTextView);
         courseDescriptionTextView = view.findViewById(R.id.courseDescriptionTextView);
-        btnEdit = view.findViewById(R.id.btn_courseDetails_edit);
-        btnDelete = view.findViewById(R.id.btn_courseDetails_delete);
-
-        btnEdit.setOnClickListener(this);
-        btnDelete.setOnClickListener(this);
 
         mPrefs = getActivity().getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
         String courseCode = getArguments().getString(BUNDLE_COURSE_CODE);
@@ -91,19 +79,5 @@ public class ManagerCourseDetailsFragment extends Fragment implements View.OnCli
                 .replace("]", ""));
         pathwayTextView.setText(getPathwayLabel(mSelectedCourse.getPathway()));
         courseDescriptionTextView.setText(mSelectedCourse.getDescription());
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_courseDetails_edit:
-                Bundle bundle = new Bundle();
-                bundle.putString(BUNDLE_COURSE_CODE, mSelectedCourse.getCode());
-
-                // navigate to course edit fragment
-                NavController navController = Navigation.findNavController(view);
-                navController.navigate(R.id.action_managerCourseDetailsFragment_to_managerCourseDetailsEditFragment, bundle);
-                break;
-        }
     }
 }
