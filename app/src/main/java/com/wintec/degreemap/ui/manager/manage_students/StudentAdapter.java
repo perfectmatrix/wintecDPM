ed9de371c1d30241047d4bfe2593883cba3ea1c1
Ynel Basa <marinelbasa@gmail.com>
2020-11-10 10:49:29 +1300

    Clean up colors and remove yellow spinner

--- app/src/main/java/com/wintec/degreemap/ui/manager/manage_students/StudentAdapter.java
+++ app/src/main/java/com/wintec/degreemap/ui/manager/manage_students/StudentAdapter.java
@@ -8,7 +8,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
