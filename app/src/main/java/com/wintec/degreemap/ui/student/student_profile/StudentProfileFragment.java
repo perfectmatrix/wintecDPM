f04711567b83354a23fa72609693e0ce95db98e2
Ynel Basa <marinelbasa@gmail.com>
2020-11-12 10:15:54 +1300

    Add disclaimer to profile section

--- app/src/main/java/com/wintec/degreemap/ui/student/student_profile/StudentProfileFragment.java
+++ app/src/main/java/com/wintec/degreemap/ui/student/student_profile/StudentProfileFragment.java
@@ -1,10 +1,14 @@
 package com.wintec.degreemap.ui.student.student_profile;
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -22,10 +26,12 @@ import com.wintec.degreemap.data.model.User;
 import com.wintec.degreemap.viewmodel.UserViewModel;
 
 import static android.content.Context.MODE_PRIVATE;
+import static com.wintec.degreemap.util.Constants.KEY_DISCLAIMER_ACCEPTED;
 import static com.wintec.degreemap.util.Constants.KEY_USER_KEY;
 import static com.wintec.degreemap.util.Constants.SHARED_PREFERENCES;
 
 public class StudentProfileFragment extends Fragment implements View.OnClickListener {
+    Dialog disclaimer_dialog;
 
     @Nullable
     @Override
@@ -35,6 +41,7 @@ public class StudentProfileFragment extends Fragment implements View.OnClickList
         view.findViewById(R.id.about_us).setOnClickListener(this);
         view.findViewById(R.id.developer_group).setOnClickListener(this);
         view.findViewById(R.id.student_contact_details).setOnClickListener(this);
+        view.findViewById(R.id.disclaimer).setOnClickListener(this);
 
         // load profile image from storage
         SharedPreferences prefs = getActivity().getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
@@ -69,6 +76,21 @@ public class StudentProfileFragment extends Fragment implements View.OnClickList
                 break;
             case R.id.student_contact_details:
                 NavHostFragment.findNavController(this).navigate(R.id.action_studentProfileFragment_to_contactDetailFragment);
+                break;
+            case R.id.disclaimer:
+                disclaimer_dialog = new Dialog(getContext());
+                disclaimer_dialog.setContentView(R.layout.dialog_disclaimer);
+                disclaimer_dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                Button button = disclaimer_dialog.findViewById(R.id.btn_get_started);
+                button.setText("Close");
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        disclaimer_dialog.dismiss();
+                    }
+                });
+
+                disclaimer_dialog.show();
                 break;
         }
     }
